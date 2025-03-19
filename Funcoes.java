11eaef44e2c907a415aa6e9d254d20e4c6ae32fa
Danilo Barros <130939835+danilosenati@users.noreply.github.com>
2025-03-18 22:47:33 -0300

    Create Funcoes.java
    Funcoes

---
+++ Funcoes.java
@@ -0,0 +1,14 @@
+package modulo1;
+
+public class Funcoes {
+
+	public static void main(String[] args) {
+		String nome = "Hello World";
+		saudacao(nome);
+	}
+
+	public static void saudacao(String nome) {
+		System.out.println("Nome do primeiro programa: " + nome);
+	}
+
+}
